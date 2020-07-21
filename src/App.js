8449bc73aa7435d7ef0c53a404d63ca098380daf
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-21 09:43:07 -0300

    atualizando a imagem de fundo, add o aphrodite

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firebase from './firebase';
+// import firebase from './firebase';
 
 function App() {
   return (
