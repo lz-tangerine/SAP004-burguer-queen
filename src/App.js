01cff639c9b6f9f94cb6282de4dd42b89a29b8ff
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-27 19:43:26 -0300

    Merge pull request #4 from karina1981/master

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React from 'react';
-import Background from './components/Background/Background.js';
-import logo from './imagens/logo.png';
-import './style.css';
+import React from 'react'
+import './app.css'
+import Routes from './routes'
 // import firebase from './firebase';
 
 const App = () => {
-  return (
-    <main>
-      <Background
-        idPrimary="button"
-        classNamePrimary="login"
-        namePrimary="Login"
-        idSecond="button"
-        classNameSecond="registro"
-        nameSecond="Registro"
-      />
-      <div>
-        <img src={logo} alt={'fundo imagem'} className="logotipo"></img>
-      </div>
-      <section>
-        <p className="welcome">Bem vindo CK Lover!</p>
-        <p className="message">
-          Entre com seu email e senha em Login ou Registre um novo usuário!
-        </p>
-      </section>
-    </main>
-  );
-};
+  return <Routes />
+}
 
-export default App;
+export default App
