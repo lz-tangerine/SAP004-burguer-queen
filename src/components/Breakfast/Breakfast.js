f2e9024ffc8fb99c740eb7df5c4e6862da19d025
Carol Moscheta <carolina.moscheta@gmail.com>
2020-08-09 15:01:32 -0300

    componente breakfast, pagina pedidos

---
+++ src/components/Breakfast/Breakfast.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './style.css'
+import Button from '../Button/Button.js'
+
+const fourButton = (props) => (
+  <div>
+    <p className={props.className}>{props.p}</p>
+    <div>
+      <Button
+        className={props.classNamePrimary}
+        name={props.namePrimary}
+        onClick={props.onClickPrimary}
+      />
+      <Button
+        className={props.classNameSecond}
+        name={props.nameSecond}
+        onClick={props.onClickSecond}
+      />
+      <Button
+        className={props.classNameThird}
+        name={props.nameThird}
+        onClick={props.onClickThird}
+      />
+      <Button
+        className={props.classNameFourth}
+        name={props.nameFourth}
+        onClick={props.onClickFourth}
+      />
+    </div>
+  </div>
+)
+
+export default fourButton
--- src/pages/request/index.js
+++ src/pages/request/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../../imagens/logo.png'
 import './style.css'
+import Breakfast from '../../components/Breakfast/Breakfast'
 
 const Request = () => {
   return (
@@ -12,6 +13,16 @@ const Request = () => {
         <button className="nav request"> PEDIDOS </button>
         <button className="nav preparation"> PREPARAÇÃO </button>
       </nav>
+      <div>
+        <Breakfast
+          className="breakfast"
+          p="Café da manhã"
+          classNamePrimary="americanCoffee"
+          namePrimary="Café Americano"
+          classNameSecond="expressCoffee"
+          nameSecond="Café com leite"
+        />
+      </div>
     </main>
   )
 }
