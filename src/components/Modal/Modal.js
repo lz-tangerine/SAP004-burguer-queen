f5f551a58bb70ef767e496e7112199539c4eb217
Carol Moscheta <carolina.moscheta@gmail.com>
2020-08-12 11:47:05 -0300

    modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const modal = document.getElementById('modal')
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.el = document.createElement('div')
+  }
+
+  componentDidMount() {
+    modal.appendChild(this.el)
+  }
+
+  componentWillUnmount() {
+    modal.removeChild(this.el)
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el)
+  }
+}
+export default Modal
